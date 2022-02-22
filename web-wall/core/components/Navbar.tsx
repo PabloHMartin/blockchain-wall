41d6b826db7d6c13c0519ba7f70782ee22bd2ee5
Pablo Hernandez Martin <pablohmartindev@gmail.com>
2022-02-22 20:41:00 +0100

    changes to case study

--- web-wall/core/components/Navbar.tsx
+++ web-wall/core/components/Navbar.tsx
@@ -14,8 +14,12 @@ export default function Navbar() {
                 </ul>            
             </div>
             <div className={styles.images}>
-                <img src="/github-icon.svg"/>
+                <a target='_blank' href="https://github.com/PabloHMartin/blockchain-wall">
+                    <img src="/github-icon.svg"/>
+                </a>
+                <a target='_blank' href="https://www.linkedin.com/in/pablohmartin/">
                 <img src="/linkedin-icon.svg"/>
+                </a>
             </div>
         </nav>
     );
--- web-wall/pages/case-study.tsx
+++ web-wall/pages/case-study.tsx
@@ -1,8 +1,30 @@
+import Card from "../core/components/Card";
+import styles from '../styles/Case-study.module.css';
+
+let title = 'What is cripto wall?';
+let title2 = 'Why Nextjs?';
+
+let msg = `Crypto wall is a demo project. Wall messages live in the blockchain.
+Using Truffle and NextJS we can make a twitter like wall using blockchain and web3.`;
+let msg2 = `By using NextJs we can make it work for non metamask users. 
+We use infura as blockchain access for rendering the wall from the server.`;
+
+let date = (new Date()).getTime();
+
 function CaseStudy() {
     return (  
-    <div>
-        Case study page
-    </div>
+    <main className={styles.main}>
+        <div className={styles.cards}>
+            <Card address={title} message={msg} publishDate={date}/>
+            <div className={styles.image}>
+                <img src="/eth.svg" />
+            </div>
+            <Card address={title2} message={msg2} publishDate={date}/>
+            <div className={styles.image}>
+                <img src="/next.svg" />
+            </div>
+        </div>
+    </main>
     );
 }
 
